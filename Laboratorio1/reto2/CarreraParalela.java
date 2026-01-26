49e528471d0957fe1a57d1f94b1292a695ee7c17
Luiza Gonzalez <lulogove42.gmail.com>
2026-01-25 22:27:01 -0500

    feature: Estructura base CarreraParalela

---
+++ Laboratorio1/reto2/CarreraParalela.java
@@ -0,0 +1,12 @@
+package reto2;
+
+import java.util.List;
+
+public class CarreraParalela
+{
+    public static void main(String[] args)
+    {
+        System.out.println("*****Carrera en paralelo-LJE*****")
+    }
+
+}
