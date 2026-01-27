558937d0f21a498c6616a2eaf42f66d7a8b0edc5
Juan Roa <juan.roa-h@mail.escuelaing.edu.co>
2026-01-27 13:30:21 -0500

    funcion maximo

--- Laboratorio1/reto2/CarreraParalela.java
+++ Laboratorio1/reto2/CarreraParalela.java
@@ -2,11 +2,18 @@ package reto2;
 
 import java.util.List;
 
+
 public class CarreraParalela
 {
     public static void main(String[] args)
     {
         System.out.println("Carrera en paralelo-LJE")
+    }
+
+    public static Integer numeroMax(List<Integer> lista){
+        Function<List<Integer>,Integer> maximo = arreglo -> arreglo.stream().max(Integer::compareTo);
+        Integer max = maximo.apply(lista);
+        return max;
     }
 
 }
