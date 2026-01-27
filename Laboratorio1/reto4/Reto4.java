49c9158667522090b7e40a04a3cdde714418adae
Luiza Gonzalez <lulogove42.gmail.com>
2026-01-26 22:45:41 -0500

    feature: implementación Hashtable - Reto 4 Estudiante B

--- Laboratorio1/reto4/Reto4.java
+++ Laboratorio1/reto4/Reto4.java
@@ -3,14 +3,20 @@ import java.util.*;
 
 public class Reto4
 {
+    /**
+     * Método que crea un Hashtable a partir de pares clave-valor
+     * Ignora claves duplicadas (conserva el primer valor encontrado)
+     * Asegura la sincronización
+     * @param treasureMap
+     * @return la hashtable creada
+     */
     public static Hashtable<String, Integer> createHashtableB(Map<String, Integer> treasureMap)
     {
-        Hashtable<String, Integer> table = new Hashtable<>();
-        for (Map.Entry<String,Integer> entrada : treasureMap.entrySet())
-        {
-            table.putIfAbsent(entrada.getKey(),entrada.getValue());
-        }
-        return table;
+        Hashtable<String, Integer> hashtable = new Hashtable<>();
+        // Usar lambda para procesar cada par
+        treasureMap.forEach((key, value) -> {hashtable.putIfAbsent(key, value);
+        });
+        return hashtable;
     }
 
     public static void main(String[] args)
@@ -23,7 +29,7 @@ public class Reto4
 
         Hashtable<String, Integer> resultMap = createHashtableB(mapaEntrada);
 
-        System.out.println("***Hashtable - EB***");
+        System.out.println("Hashtable - EB");
         resultMap.forEach((k,v)-> System.out.println(k + "|" + v));
     }
 }
