d9ea716b8862f639add950bbf6656dc3ad41721c
Juan Roa <juan.roa-h@mail.escuelaing.edu.co>
2026-01-27 15:15:06 -0500

    vuelta 2 funcion multiplo de 2

--- Laboratorio1/reto2/CarreraParalela.java
+++ Laboratorio1/reto2/CarreraParalela.java
@@ -9,7 +9,7 @@ public class CarreraParalela
     public static void main(String[] args)
     {
         System.out.println("Carrera en paralelo-LJE");
-        List lista = new ArrayList(Arrays.asList(4,43,11,4,6,7,1));
+        List lista = new ArrayList(Arrays.asList(4,42,11,4,6,7,1));
         calculoResultado(lista).imprimir();
     }
 
@@ -29,11 +29,18 @@ public class CarreraParalela
         Integer cant = cantidad.apply(lista);
         return cant;
     }
+
+    public static boolean verificarMultiploDeDos(Integer numero){
+        boolean verificacion = (numero % 2 == 0) ? true : false;
+        return verificacion;
+    }
+
     public static Resultado calculoResultado(List<Integer> lista){
         Integer cant = cantidadElementos(lista);
         Integer max = numeroMax(lista);
         Integer min = numeroMin(lista);
-        Resultado calculo = new Resultado(cant, max, min);
+        boolean verificacion = verificarMultiploDeDos(max);
+        Resultado calculo = new Resultado(max, min, cant, verificacion);
         return calculo;
     }
 
@@ -41,17 +48,20 @@ public class CarreraParalela
         public Integer maximo;
         public Integer minimo;
         public Integer cantidad;
+        public boolean maximoMultiploDeDos;
 
-        public Resultado(Integer maximo, Integer minimo, Integer cantidad) {
+        public Resultado(Integer maximo, Integer minimo, Integer cantidad, boolean maximoMultiploDeDos) {
             this.maximo = maximo;
             this.minimo = minimo;
             this.cantidad = cantidad;
+            this.maximoMultiploDeDos = maximoMultiploDeDos;
         }
 
         public void imprimir() {
             System.out.println("Maximo: " + maximo);
             System.out.println("Minimo: " + minimo);
             System.out.println("Cantidad: " + cantidad);
+            System.out.println("MaximoMultiploDeDos: " + maximoMultiploDeDos);
         }
     }
 }
