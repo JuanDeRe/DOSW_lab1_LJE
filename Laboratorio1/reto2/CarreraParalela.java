1ab8a512b769bfca7c209f13bb96a26fbf521e41
Luiza Gonzalez <lulogove42.gmail.com>
2026-01-26 21:22:35 -0500

    feature: estructura base CarreraParalela.java

--- Laboratorio1/reto2/CarreraParalela.java
+++ Laboratorio1/reto2/CarreraParalela.java
@@ -6,7 +6,7 @@ public class CarreraParalela
 {
     public static void main(String[] args)
     {
-        System.out.println("*****Carrera en paralelo-LJE*****")
+        System.out.println("Carrera en paralelo-LJE")
     }
 
 }
